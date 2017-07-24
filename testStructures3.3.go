0b03b375b5058c198195a195db1ca07f7980f36f
Leonardo Gómez Sosa <legogomezs@gmail.com>
2017-07-24 02:02:43 -0500

    Probando PDF

--- testStructures3.3.go
+++ testStructures3.3.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 
+	"github.com/jung-kurt/gofpdf"
+
 	"./XMLStructures"
 )
 
@@ -10,5 +12,12 @@ func main() {
 	var comprobante xmlstructures.Comprobante
 	estructura := xmlstructures.MarshallData2XML(comprobante)
 	fmt.Println(estructura)
-
+	pdf := gofpdf.New("P", "mm", "A4", "")
+	pdf.AddPage()
+	pdf.SetFont("Arial", "", 11)
+	pdf.Cell(40, 10, "Hello, world")
+	err := pdf.OutputFileAndClose("hello.pdf")
+	if err != nil {
+		panic(err)
+	}
 }
